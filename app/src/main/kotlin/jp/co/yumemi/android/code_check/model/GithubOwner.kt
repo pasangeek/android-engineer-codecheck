55e9e38294afbd301352a2c37f63efa37b237cd9
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-06-18 00:13:41 +0900

    github owner class added

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/model/GithubOwner.kt
@@ -0,0 +1,8 @@
+package jp.co.yumemi.android.code_check.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Owner(@SerializedName("avatar_url") val avatarUrl: String) : Parcelable

b52e5c91425ef5d8c9351f65f19ed115ea8e466e
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-06-17 22:37:05 +0900

    viewmodel seperated -firstphase complete-

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneFragment.kt
@@ -10,49 +10,63 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
 import jp.co.yumemi.android.code_check.adapters.CustomAdapter
 import jp.co.yumemi.android.code_check.databinding.FragmentOneBinding
 
-import jp.co.yumemi.android.code_check.model.GithubRepositoryData
 
-class OneFragment: Fragment(R.layout.fragment_one){
+import jp.co.yumemi.android.code_check.model.GithubRepositoryData
+import jp.co.yumemi.android.code_check.repository.GithubRepository
+
+class OneFragment: Fragment(){
+
+
+    lateinit var _binding: FragmentOneBinding
+    lateinit var viewModel: OneViewModel
+    lateinit var customAdapter: CustomAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOneBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(requireActivity())[OneViewModel::class.java]
+        _binding.vm = viewModel
+        _binding.lifecycleOwner = this
+
+        return _binding.root
+
+    }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding= FragmentOneBinding.bind(view)
-
-        val oneViewModel= OneViewModel(requireContext())
-
-        val layoutManager= LinearLayoutManager(requireContext())
-        val dividerItemDecoration=
-            DividerItemDecoration(requireContext(), layoutManager.orientation)
-        val adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener{
-            override fun itemClick(item: GithubRepositoryData){
+      customAdapter = CustomAdapter(object : CustomAdapter.OnItemClickListener {
+            override fun itemClick(item: GithubRepositoryData) {
                 gotoRepositoryFragment(item)
             }
         })
 
-        binding.searchInputText
-            .setOnEditorActionListener{ editText, action, _ ->
-                if (action== EditorInfo.IME_ACTION_SEARCH){
+        _binding.searchInputText
+            .setOnEditorActionListener { editText, action, _ ->
+                if (action == EditorInfo.IME_ACTION_SEARCH) {
                     editText.text.toString().let {
-                        oneViewModel.searchResults(it).apply{
-                            adapter.submitList(this)
-                        }
+                        viewModel.searchResults(it)
                     }
                     return@setOnEditorActionListener true
                 }
                 return@setOnEditorActionListener false
             }
 
-        binding.recyclerView.also{
-            it.layoutManager= layoutManager
-            it.addItemDecoration(dividerItemDecoration)
-            it.adapter= adapter
+        _binding.recyclerView.adapter = customAdapter
+        viewModel.gitHubRepositoryList.observe(requireActivity()) {
+            customAdapter.submitList(it)
         }
     }
 

b8c48aebd6ea1de54f4d65558af94468271f42ba
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-06-18 21:29:03 +0900

    fix string concatenation

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/fragments/RepositoryDetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/fragments/RepositoryDetailFragment.kt
@@ -12,6 +12,9 @@ import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.RepositoryDetailBinding
 
 
+/**
+ * A fragment that displays the details of a repository.
+ */
 class RepositoryDetailFragment : Fragment(R.layout.repository_detail) {
 
     private val args: RepositoryDetailFragmentArgs by navArgs()
@@ -23,7 +26,7 @@ class RepositoryDetailFragment : Fragment(R.layout.repository_detail) {
         super.onViewCreated(view, savedInstanceState)
 
         //Log.d("検索した日時", lastSearchDate.toString())//null pointer
-//TODO:must add postfix string concatenation
+
         binding = RepositoryDetailBinding.bind(view)
         // Retrieve the passed item from the arguments
         val item = args.item
@@ -34,12 +37,12 @@ class RepositoryDetailFragment : Fragment(R.layout.repository_detail) {
         // Set the programming language
         _binding.languageView.text = item.language
         // Display the number of stars
-        _binding.starsView.text = "${item.stargazersCount} stars"
+        _binding.starsView.text = getString(R.string.stars_count, item.stargazersCount)
         // Display the number of watchers
-        _binding.watchersView.text = "${item.watchersCount} watchers"
+        _binding.watchersView.text = getString(R.string.watchers_count, item.watchersCount)
         // Display the number of forks
-        _binding.forksView.text = "${item.forksCount} forks"
+        _binding.forksView.text = getString(R.string.forks_count, item.forksCount)
         // Display the number of open issues
-        _binding.openIssuesView.text = "${item.openIssuesCount} open issues"
+        _binding.openIssuesView.text = getString(R.string.open_issues_count, item.openIssuesCount)
     }
 }
